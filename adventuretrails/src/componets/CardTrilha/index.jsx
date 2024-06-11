7a37b208af051f9d735a22216f79d8ea5a081c19
Daniel Bartelli <danielbartelli27@gmail.com>
2024-06-11 05:01:58 -0300

    ajust props

--- adventuretrails/src/componets/CardTrilha/index.jsx
+++ adventuretrails/src/componets/CardTrilha/index.jsx
@@ -1,3 +1,5 @@
+import * as PropTypes from "prop-types"
+
 function CardTrilha({dadosTrilha}) {
     return (
     <>
@@ -6,6 +8,20 @@ function CardTrilha({dadosTrilha}) {
     <img width={200} src={dadosTrilha.urlImagem} alt="imagem trilha"/>
     </>
     )
+}
+
+CardTrilha.propType = {
+    dadosTrilha: PropTypes.exact ({
+        nomeTrilha: PropTypes.string.isRequired,
+        cidade: PropTypes.string.isRequired,
+        estado: PropTypes.string.isRequired,
+        duracao: PropTypes.number.isRequired,
+        trajeto: PropTypes.number.isRequired,
+        dificuldade: PropTypes.string.isRequired,
+        tipo: PropTypes.string.isRequired,
+        nomeUsuario: PropTypes.string.isRequired,
+        urlImagem: PropTypes.string.isRequired 
+    })
 }
 
 export default CardTrilha;
